387ce693c5e10472befdd66d1682bf5bca343008
Cheroit <maksimermakov1337@yandex.ru>
2023-10-30 20:11:29 +0300

    реализованы две функции шифровки и расшифровки

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -9,7 +9,39 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    ALPHABET_LENGTH = 26
+
+    len_keyword = len(keyword)
+    len_plaintext = len(plaintext)
+
+    if len_keyword < len_plaintext:
+        n_repeat = (len_plaintext // len_keyword) + 1
+        keyword = keyword * n_repeat
+        keyword = keyword[:len_plaintext]
+
+    for ch1, ch2 in zip(plaintext, keyword):
+        # for i in range(len(plaintext)):
+        #     ch1 = plaintext[i]
+        #     ch2 = keyword[i]
+
+        if ch1.islower() and ch2.islower():
+            ciphered_ch = chr(ord('a') + (ord(ch1) - ord('a') + ord(ch2) - ord('a')) % ALPHABET_LENGTH)
+
+        elif ch1.isupper() and ch2.islower():
+            ciphered_ch = chr(ord('A') + (ord(ch1) - ord('A') + ord(ch2) - ord('a')) % ALPHABET_LENGTH)
+
+        elif ch1.islower() and ch2.isupper():
+            ciphered_ch = chr(ord('a') + (ord(ch1) - ord('a') + ord(ch2) - ord('A')) % ALPHABET_LENGTH)
+
+        elif ch1.isupper() and ch2.isupper():
+            ciphered_ch = chr(ord('A') + (ord(ch1) - ord('A') + ord(ch2) - ord('A')) % ALPHABET_LENGTH)
+
+        else:
+            ciphered_ch = ch1
+
+        ciphertext += ciphered_ch
+
     return ciphertext
 
 
@@ -24,5 +56,34 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    ALPHABET_LENGTH = 26
+
+    len_keyword = len(keyword)
+    len_ciphertext = len(ciphertext)
+
+    if len_keyword < len_ciphertext:
+        n_repeat = (len_ciphertext // len_keyword) + 1
+        keyword = keyword * n_repeat
+        keyword = keyword[:len_ciphertext]
+
+    for ch1, ch2 in zip(ciphertext, keyword):
+
+        if ch1.islower() and ch2.islower():
+            ciphered_ch = chr(ord('a') + (ord(ch1) - ord('a') - ord(ch2) + ord('a')) % ALPHABET_LENGTH)
+
+        elif ch1.islower() and ch2.isupper():
+            ciphered_ch = chr(ord('a') + (ord(ch1) - ord('a') - ord(ch2) + ord('A')) % ALPHABET_LENGTH)
+
+        elif ch1.isupper() and ch2.islower():
+            ciphered_ch = chr(ord('A') + (ord(ch1) - ord('A') - ord(ch2) + ord('a')) % ALPHABET_LENGTH)
+
+        elif ch1.isupper() and ch2.isupper():
+            ciphered_ch = chr(ord('A') + (ord(ch1) - ord('A') - ord(ch2) + ord('A')) % ALPHABET_LENGTH)
+
+        else:
+            ciphered_ch = ch1
+
+        plaintext += ciphered_ch
+
     return plaintext
